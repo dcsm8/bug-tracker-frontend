2ab6fcce84ef99a26958ef0ed5f4e71a09593811
David Sánchez <deividsanchez96@hotmail.com>
2022-04-22 10:43:37 -0500

    Use process.env to get API_URL

--- api/auth.ts
+++ api/auth.ts
@@ -1,12 +1,19 @@
 import axios from "axios";
 
+const apiClient = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+    "Content-type": "application/json",
+  },
+});
+
 export const authenticate = async (data: any) => {
   const payload = {
     username: data.email,
     password: data.password,
   };
-  const response = await axios.post<{ token: string }>(
-    "https://bug-tracker-backend-one.herokuapp.com/api/auth/login",
+  const response = await apiClient.post<{ token: string }>(
+    "auth/login",
     payload
   );
 
