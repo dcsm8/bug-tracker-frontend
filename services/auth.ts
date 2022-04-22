5ccba8ee9b47d706866d63a8fdee3a0ac9641eac
David Sánchez <deividsanchez96@hotmail.com>
2022-04-22 17:22:32 -0500

    Change path

--- services/auth.ts
+++ services/auth.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { LoginDto, TokenResponse } from "../components/auth/login-form/types";
+import { LoginDto } from "../components/auth/login-form/types";
 
 const apiClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
@@ -9,6 +9,6 @@ const apiClient = axios.create({
 });
 
 export const authenticate = async (data: LoginDto) => {
-  const response = await apiClient.post("auth/login", data);
+  const response = await apiClient.post("/auth/login", data);
   return response.data;
 };
