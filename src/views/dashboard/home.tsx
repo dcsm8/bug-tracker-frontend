7f8e99b8aa7edd1fd932d14b0c711e2fae1063f0
David Sánchez <deividsanchez96@hotmail.com>
2022-05-08 18:40:59 -0500

    add asseinfo/react-kanban

--- src/views/dashboard/home.tsx
+++ src/views/dashboard/home.tsx
@@ -1,5 +1,51 @@
-function Home() {
-  return <h2>Home</h2>;
-}
+import Board from '@asseinfo/react-kanban';
+import '@asseinfo/react-kanban/dist/styles.css';
+
+const board = {
+  columns: [
+    {
+      id: 1,
+      title: 'Backlog',
+      cards: [
+        {
+          id: 1,
+          title: 'Add card',
+          description: 'Add capability to add a card in a column',
+        },
+        {
+          id: 3,
+          title: 'Add card',
+          description: 'Add capability to add a card in a column',
+        },
+      ],
+    },
+    {
+      id: 2,
+      title: 'Doing',
+      cards: [
+        {
+          id: 2,
+          title: 'Drag-n-drop support',
+          description: 'Move a card between the columns',
+        },
+      ],
+    },
+    {
+      id: 3,
+      title: 'Testing',
+      cards: [
+        {
+          id: 5,
+          title: 'Drag-n-drop support',
+          description: 'Move a card between the columns',
+        },
+      ],
+    },
+  ],
+};
+
+const Home = () => {
+  return <Board initialBoard={board} />;
+};
 
 export default Home;
