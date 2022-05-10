107ca181d0eb76c977ee4b1344cffd2dbe497006
DAVID SANCHEZ <deividsanchez96@hotmail.com>
2022-05-10 02:39:55 -0500

    Add path aliases config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
+import { ViteAliases } from 'vite-aliases';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [ViteAliases(), react()],
 });
