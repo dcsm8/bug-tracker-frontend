e0e7189ce93485b1152036d7ea1ad2f6538aad15
David Sánchez <deividsanchez96@hotmail.com>
2022-05-25 22:52:30 -0500

    Add nunito-sans font

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import '@fontsource/nunito-sans/200.css';
+import '@fontsource/nunito-sans/300.css';
+import '@fontsource/nunito-sans/400.css';
+import '@fontsource/nunito-sans/600.css';
+import '@fontsource/nunito-sans/700.css';
+import '@fontsource/nunito-sans/800.css';
+import '@fontsource/nunito-sans/900.css';
+
 import './index.css';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { UserProvider } from './store/use-user';
 import { ReactQueryDevtools } from 'react-query/devtools';
+
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  fonts: {
+    heading: `'Nunito Sans', sans-serif`,
+    body: `'Nunito Sans', sans-serif`,
+  },
+});
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -23,7 +41,7 @@ ReactDOM.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <UserProvider>
           <App />
         </UserProvider>
