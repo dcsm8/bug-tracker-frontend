f77ce2435da104024076ae87841dec742403c84e
David Sánchez <deividsanchez96@hotmail.com>
2022-05-25 20:39:29 -0500

    Create delete confirm dialog component

---
+++ src/components/delete-confirm-dialog/delete-confirm-dialog.tsx
@@ -0,0 +1,55 @@
+import {
+  AlertDialog,
+  AlertDialogOverlay,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogBody,
+  AlertDialogFooter,
+  Button,
+} from '@chakra-ui/react';
+import React from 'react';
+
+type DeleteConfirmDialogProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  onDelete: () => void;
+  title?: string;
+};
+
+export const DeleteConfirmDialog = ({
+  isOpen,
+  onClose,
+  onDelete,
+  title,
+}: DeleteConfirmDialogProps) => {
+  const cancelRef = React.useRef<any>();
+
+  return (
+    <AlertDialog
+      isOpen={isOpen}
+      leastDestructiveRef={cancelRef}
+      onClose={onClose}
+    >
+      <AlertDialogOverlay>
+        <AlertDialogContent>
+          <AlertDialogHeader fontSize='lg' fontWeight='bold'>
+            {title || 'Delete'}
+          </AlertDialogHeader>
+
+          <AlertDialogBody>
+            Are you sure? You can't undo this action afterwards.
+          </AlertDialogBody>
+
+          <AlertDialogFooter>
+            <Button ref={cancelRef} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button colorScheme='red' onClick={onDelete} ml={3}>
+              Delete
+            </Button>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialogOverlay>
+    </AlertDialog>
+  );
+};
