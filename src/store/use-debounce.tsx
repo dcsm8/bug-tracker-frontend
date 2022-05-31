edd7cd3a782a6e253b4e94a681aca5d7871323ea
David Sánchez <deividsanchez96@hotmail.com>
2022-05-31 00:08:39 -0500

    Use arrow function

--- src/store/use-debounce.tsx
+++ src/store/use-debounce.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 
 // see https://github.com/tannerlinsley/react-query/issues/293
 // see https://usehooks.com/useDebounce/
-export default function useDebounce(value, delay) {
+export const useDebounce = (value, delay) => {
   // State and setters for debounced value
   const [debouncedValue, setDebouncedValue] = useState(value);
 
@@ -24,4 +24,4 @@ export default function useDebounce(value, delay) {
   );
 
   return debouncedValue;
-}
+};
