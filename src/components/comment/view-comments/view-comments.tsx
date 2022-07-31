d269ae157eada0aa3abfb7cb8095c31bbcb51c5d
David Sánchez <deividsanchez96@hotmail.com>
2022-07-31 01:01:39 -0500

    Add delete comment feature

--- src/components/comment/view-comments/view-comments.tsx
+++ src/components/comment/view-comments/view-comments.tsx
@@ -1,13 +1,39 @@
-import { Avatar, Box, Divider, Flex, HStack, Text } from '@chakra-ui/react';
+import {
+  Avatar,
+  Box,
+  Button,
+  Divider,
+  Flex,
+  HStack,
+  Text,
+  useDisclosure,
+} from '@chakra-ui/react';
+import { DeleteConfirmDialog } from '@components/delete-confirm-dialog/delete-confirm-dialog';
 import { Comment } from '@interfaces/comment.interface';
 import { Task } from '@interfaces/task-interface';
+import { remove } from '@services/comments-service';
 import React from 'react';
+import { useMutation, useQueryClient } from 'react-query';
 
 type ViewCommentProps = {
   comment: Comment;
 };
 
 const ViewComment = ({ comment }: ViewCommentProps) => {
+  const {
+    isOpen: isOpenDelete,
+    onOpen: onOpenDelete,
+    onClose: onCloseDelete,
+  } = useDisclosure();
+
+  const queryClient = useQueryClient();
+
+  const { mutateAsync: deleteComment } = useMutation(remove, {
+    onSuccess: () => {
+      queryClient.invalidateQueries(['tasks', comment.task]);
+    },
+  });
+
   return (
     <React.Fragment>
       <Flex mt={4}>
@@ -25,6 +51,28 @@ const ViewComment = ({ comment }: ViewCommentProps) => {
             </Text>
           </HStack>
           <div dangerouslySetInnerHTML={{ __html: comment.text }} />
+          <HStack mr={5} mt={1}>
+            <Button colorScheme='teal' size='xs' variant='link'>
+              Edit
+            </Button>
+            <Text fontSize='x-small' textColor='gray.400'>
+              &#x2022;
+            </Text>
+            <Button
+              colorScheme='teal'
+              size='xs'
+              variant='link'
+              onClick={onOpenDelete}
+            >
+              Delete
+            </Button>
+            <DeleteConfirmDialog
+              isOpen={isOpenDelete}
+              onClose={onCloseDelete}
+              onDelete={() => deleteComment(comment)}
+              title='Delete Comment'
+            />
+          </HStack>
         </Box>
       </Flex>
       <Divider mt={3} />
