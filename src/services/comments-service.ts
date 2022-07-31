b8c60a39acd164f6f0751a352f4d6263dc68b322
David Sánchez <deividsanchez96@hotmail.com>
2022-07-31 01:01:21 -0500

    Add remove endpoint

--- src/services/comments-service.ts
+++ src/services/comments-service.ts
@@ -6,3 +6,8 @@ export const create = async (comment: CreateCommentDto): Promise<Comment> => {
   const response = await apiClient.post<Comment>('/comments', comment);
   return response.data;
 };
+
+export const remove = async (comment: Partial<Comment>): Promise<Comment> => {
+  const response = await apiClient.delete<Comment>(`/comments/${comment.id}`);
+  return response.data;
+};
